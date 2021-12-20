e556ff7e68853de6683320bf0ec4811d1ec738f9
Mackmiller <mackmiller19@gmail.com>
2021-12-20 16:29:45 -0500

    pull in total supply number

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,17 @@ const Home = (props) => {
     // return 1 individual meme coin name from array
     // console.log(props.coins.coins[0].name)
 
+    // return the name of each coin using map
+    const names = props.coins.coins.map(coin=>{
+        // console.log("this is coin name and circulating supply", coin.name, coin.total_supply)
+        return <li>{coin.name}, $ {coin.total_supply}</li>
+    })
+
 	return (
 		<div>
+			<h1>Hello</h1>
             <h1>{props.coins.name} Tokens</h1>
-            <ul>{ 
-                props.coins.coins.map(coin=>{
-                    //console.log("this is coin name", coin.name)
-                    return <li>{coin.name}</li>
-                })
-            }</ul>
+            <ul>{names}</ul>
 		</div>
 	)
 }
