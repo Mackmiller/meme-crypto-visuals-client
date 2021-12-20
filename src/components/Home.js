c3d25850b622f0f30d7fdc0682f44f160bf376a8
Mackmiller <mackmiller19@gmail.com>
2021-12-20 16:03:06 -0500

    called list of meme coins from api and displayed on home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 const Home = (props) => {
 
+    //returns called array of individual meme coins
+    // console.log(props.coins.coins)
+
+    // return 1 individual meme coin name from array
+    // console.log(props.coins.coins[0].name)
+
 	return (
 		<div>
-			<h1>Hello</h1>
-            <h1>{props.coins.name}</h1>
+            <h1>{props.coins.name} Tokens</h1>
+            <ul>{ 
+                props.coins.coins.map(coin=>{
+                    //console.log("this is coin name", coin.name)
+                    return <li>{coin.name}</li>
+                })
+            }</ul>
 		</div>
 	)
 }
