b0c4e47d4a27c75e7ef613ecc524670d1c1cea55
Mackmiller <mackmiller19@gmail.com>
2021-12-22 12:04:17 -0500

    bar chart functioning

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+  } from "recharts";
 const axios = require('axios')
 const Home = () => {
     
@@ -11,7 +21,7 @@ const Home = () => {
         axios.get(url)
             .then(response => {
                 const coinData = response.data.data.coins
-                console.log('this is coin data', coinData);
+                // console.log('this is coin data', coinData);
                 setCoins(coinData)
             })
             .catch(err => console.log(err))
@@ -36,31 +46,55 @@ const Home = () => {
     const marketCap = coins.map((c, i)=>{
         // establishing each key/value pair:
         const name = c.name
+        const symbol = c.symbol
         const capacity = c.quote.USD.market_cap;
         const updated = c.quote.USD.last_updated;
         return (
-            {name, capacity, updated}
+            {name, capacity, updated, symbol}
         ) 
     })
-    // console.log("this is market cap data", marketCap)
+    console.log("this is market cap data", marketCap)
 
       // 24h volume data for visual
       const dailyVolume = coins.map((c, i)=>{
         // establishing each key/value pair:
         const name = c.name
+        const symbol = c.symbol
         const volume = c.quote.USD.volume_24h;
         const updated = c.quote.USD.last_updated;
         return (
-            {name, volume, updated}
+            {name, symbol, volume, updated}
         ) 
     })
     // console.log("this is 24h volume data", dailyVolume)
 
 	return (
-		<div>
-			<h1>Top Meme Tokens</h1>
-            {allCoins}
-		</div>
+        <div>
+            <div>
+                <h1>Top Meme Tokens</h1>
+                {allCoins}
+            </div>
+            {/* <ResponsiveContainer> */}
+                <BarChart
+                    width={1000}
+                    height={500}
+                    data={marketCap}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 200,
+                        bottom: 5
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="symbol" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="capacity" fill="#8884d8" />
+                </BarChart>
+            {/* </ResponsiveContainer> */}
+        </div>
 	)
 }
 
