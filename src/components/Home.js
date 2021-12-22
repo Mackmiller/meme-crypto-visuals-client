0fc0bb59d8de3419303f90507ba647321b5a5b7c
Mackmiller <mackmiller19@gmail.com>
2021-12-22 11:06:07 -0500

    created data objects from mappings

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,33 +32,34 @@ const Home = () => {
         ) 
     })
 
-    // coins visuals data
-    const coinVisuals = coins.map((c, i)=>{
-
-        // object creating for each key/value pair:
-        const cap = c.quote.USD.market_cap;
+    // marketcap data for visual
+    const marketCap = coins.map((c, i)=>{
+        // establishing each key/value pair:
+        const name = c.name
+        const capacity = c.quote.USD.market_cap;
         const updated = c.quote.USD.last_updated;
-        const capAndUpdated = {updated, cap};
-        console.log(typeof(capAndUpdated))
         return (
-            <li key={i} style={{listStyle: "none"}}>
-                <div className="coinVisuals">
-                    <div className="coinData">
-                        <h1>{c.name}</h1>
-                        <div>Last Updated: {c.quote.USD.last_updated}</div>
-                        <div>Market Cap: $ {c.quote.USD.market_cap}</div>
-                        <div>24 Hour Volume $ {c.quote.USD.volume_24h}</div>
-                    </div>
-                </div>
-            </li>
+            {name, capacity, updated}
         ) 
     })
+    // console.log("this is market cap data", marketCap)
+
+      // 24h volume data for visual
+      const dailyVolume = coins.map((c, i)=>{
+        // establishing each key/value pair:
+        const name = c.name
+        const volume = c.quote.USD.volume_24h;
+        const updated = c.quote.USD.last_updated;
+        return (
+            {name, volume, updated}
+        ) 
+    })
+    // console.log("this is 24h volume data", dailyVolume)
 
 	return (
 		<div>
 			<h1>Top Meme Tokens</h1>
             {allCoins}
-            {coinVisuals}
 		</div>
 	)
 }
