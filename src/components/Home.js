439e106f93f0da16d46c97e7c4b7c395bb0ef238
Mackmiller <mackmiller19@gmail.com>
2021-12-21 21:11:47 -0500

    created object key value pair for each market cap and quote pair

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,16 +16,44 @@ const Home = () => {
             .catch(err => console.log(err))
 	}, [])
 
+    // coins text data
     const allCoins = coins.map((c, i)=>{
-        // e.preventDefault()
         return (
             <li key={i} style={{listStyle: "none"}}>
                 <div className="allCoins">
                     <div className="coinInfo">
                         <h1>{c.name}</h1>
-                        <div>Market Cap: $ {c.quote.USD.market_cap}</div>
                         <div>Price: $ {c.quote.USD.price}</div>
                         <div>Circulating Supply: {c.circulating_supply} {c.symbol}</div>
+                    </div>
+                </div>
+            </li>
+        ) 
+    })
+
+    // coins visuals data
+    const coinVisuals = coins.map((c, i)=>{
+        // array test:
+        // let newArray = []
+        // newArray.push([c.quote.USD.market_cap])
+
+        // object creating for each key/value pair:
+        const cap = c.quote.USD.market_cap;
+        const updated = c.quote.USD.last_updated;
+        const capAndUpdated = {updated, cap};
+        console.log(capAndUpdated)
+
+
+
+        // console.log(newArray)
+        return (
+            <li key={i} style={{listStyle: "none"}}>
+                <div className="coinVisuals">
+                    <div className="coinData">
+                        <h1>{c.name}</h1>
+                        <div>Last Updated: {c.quote.USD.last_updated}</div>
+                        <div>Market Cap: $ {c.quote.USD.market_cap}</div>
+                        <div>24 Hour Volume $ {c.quote.USD.volume_24h}</div>
                     </div>
                 </div>
             </li>
@@ -36,6 +64,7 @@ const Home = () => {
 		<div>
 			<h1>Top Meme Tokens</h1>
             {allCoins}
+            {/* {coinVisuals} */}
 		</div>
 	)
 }
