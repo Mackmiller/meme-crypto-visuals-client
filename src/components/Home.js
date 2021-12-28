64404abee04e421329527fc78025469a1f34a38a
Mackmiller <mackmiller19@gmail.com>
2021-12-27 20:25:19 -0500

    all ten logo images appear on page remder

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import {
     Legend,
     ResponsiveContainer,
   } from "recharts";
+
 const axios = require('axios')
 const Home = () => {
     
@@ -17,79 +18,75 @@ const Home = () => {
     let [coins, setCoins] = useState([])
     const [loading, setLoading] = useState(false);
 	let url = "http://localhost:8000"
-  // run api and store coin data
-	useEffect(() => {
-        getData()
-        getIds()
-        ids? getLogos() : console.log("ids don't exist")
-	}, [])
 
     // 1
-    const getData = () => {
+    // run api and store coin data
+	useEffect(() => {
         setLoading(true)
         axios.get(url)
             .then(response => {
                 const coinData = response.data.data.coins
-                // console.log('this is coin data', coinData);
                 setCoins(coinData)
                 setLoading(false)
             })
+            // .then(newResult=> getIds(newResult))
+            // .then(finalResult=>getLogos(finalResult))
             .catch(err => {
                 console.log(err)
                 setLoading(false);
             })
-    }
+	}, [url])
+
     // 2
-   const getIds = () => {
-        // coins id data
-        const coinIds = coins.map((c, i)=>{
-            let id = c.id
-            return(
-                {id}
-            )
-        })
-        setIds(coinIds)
-    }
+    useEffect(() => {
+        if (coins){
+            // coins id data
+            const coinIds = coins.map((c, i)=>{
+                let id = c.id
+                return(
+                    {id}
+                )
+            })
+            setIds(coinIds)
+            // console.log('this is ids', ids)
+        }
+    },[coins])
 
     // 3
-    const getLogos = () => {
-        console.log("this is state ids: ", ids)
-        ids.map((logoId, i)=>{
-            // console.log("this is state ids", ids)
-            axios.get(`${url}/cryptocoin/${logoId.id}`)
-                .then(response => {
-                    // let logoLinks = response.data.data[logoId.id].logo
-                    let logoLinks = response.data.data[logoId.id]
-                    console.log("logoLinks: ", [logoLinks])
-                    setLogos(logos=>[...logos, logoLinks])
-                    // setLogos(logoLinks)
-                    // console.log("here is coin logos state", logos)
-                })
-                .catch(err => console.log(err))
-        })
-    }
-
+    useEffect(() => {
+        if(ids){
+        // console.log("this is state ids: ", ids)
+            ids.map((logoId, i)=>{
+                console.log("this is state ids", ids)
+                return (
+                    axios.get(`${url}/cryptocoin/${logoId.id}`)
+                        .then(response => {
+                            // let logoLinks = response.data.data[logoId.id].logo
+                            let logoLinks = response.data.data[logoId.id]
+                            console.log("logoLinks: ", [logoLinks])
+                            setLogos(logos=>[...logos, logoLinks])
+                            // setLogos(logoLinks)
+                            // console.log("here is coin logos state", logos)
+                        })
+                        .catch(err => console.log(err))
+                )
+            })
+        }
+    },[ids, url])
+    
+    //4
     //map the logos
-    const allLinks = logos.map((l, i)=>{
+    const allLinks = 
+        logos.map((l, i)=>{
         // console.log(l.id)
-        return (
-            <li key={i} style={{listStyle: "none", display: "inline"}}>
-                <img src= {l.logo} alt="crypto logo"/>
-            </li>
-        ) 
-    })
+            return (
+                <li key={i} style={{listStyle: "none", display: "inline"}}>
+                    <img src= {l.logo} alt="crypto logo"/>
+                </li>
+            ) 
+        })
+ 
 
-    // once id state has been set and mapped, get logo images
-    // useEffect(() => {
-    //     console.log("this is coinLogos mapped", coinLogos)
-    //     // console.log(`${url}/cryptocoin/${ids[0].id}`)
-    //     // // coins id data
-    //     // axios.get(`${url}/cryptocoin/${ids[0].id}`)
-    //     //     .then(response => {
-    //     //         console.log("this is the response",response.data.data[ids[0].id].logo)
-    //     //     })
-    //     //     .catch(err => console.log(err))
-    // }, [ids])
 
     // coins text data
     const allCoins = coins.map((c, i)=>{
@@ -105,6 +102,7 @@ const Home = () => {
             </li>
         ) 
     })
+
     // marketcap data for visual
     const marketCap = coins.map((c, i)=>{
         // establishing each key/value pair:
@@ -134,52 +132,53 @@ const Home = () => {
       
     //     return null;
     //   };
+
 	return (
         <>
             {loading? (
                 <div>Loading...</div>
             ) : (
-            <div style={{textAlign: "center"}}>
-                <h1>TOP MEME TOKENS AND COINS</h1>
-                <div className="logo-display">
-                    {logos? allLinks : console.log("no data")}
+                <div style={{textAlign: "center"}}>
+                    <h1 style={{fontSize:"60px"}}>TOP MEME TOKENS AND COINS</h1>
+                    <div className="logo-display">
+                        {logos? allLinks : console.log("no data")}
+                    </div>
+                    <div className="visualization-display">
+                        <h3>Total Market Value (USD) of Circulating Supply and Volume Traded in Past 24hrs</h3>
+                        <ResponsiveContainer width="95%" height={400}>
+                            <BarChart
+                                width={1000}
+                                height={350}
+                                data={marketCap}
+                                margin={{
+                                    top: 5,
+                                    right: 30,
+                                    left: 150,
+                                    bottom: 5
+                                }}
+                                >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" style={{fontSize: "12px"}}/>
+                                <YAxis type="number"
+                                        style={{fontSize: "12px"}}
+                                        domain={[1, "auto"]}
+                                        scale="log"
+                                        orientation="left"
+                                        tickFormatter={tick => {
+                                            return `$${tick.toLocaleString()}`;
+                                        }}
+                                />
+                                <Tooltip labelFormatter={(name) => 'Name: '+name} formatter={(capacity) =>'$'+capacity.toLocaleString("en-US")} />
+                                <Legend />
+                                <Bar name="Market Capacity" dataKey="capacity" fill="#B48DD8" />
+                                <Bar name="Volume Traded (24h)" dataKey="volume" fill="#694BA0" />
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
+                    <div className="stats-display">
+                        {allCoins}
+                    </div>
                 </div>
-                <div className="visualization-display">
-                    <h3>Total Market Value (USD) of Circulating Supply and Volume Traded in Past 24hrs</h3>
-                    <ResponsiveContainer width="95%" height={400}>
-                        <BarChart
-                            width={1000}
-                            height={350}
-                            data={marketCap}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 150,
-                                bottom: 5
-                            }}
-                            >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" style={{fontSize: "12px"}}/>
-                            <YAxis type="number"
-                                    style={{fontSize: "12px"}}
-                                    domain={[1, "auto"]}
-                                    scale="log"
-                                    orientation="left"
-                                    tickFormatter={tick => {
-                                        return `$${tick.toLocaleString()}`;
-                                    }}
-                            />
-                            <Tooltip labelFormatter={(name) => 'Name: '+name} formatter={(capacity) =>'$'+capacity.toLocaleString("en-US")} />
-                            <Legend />
-                            <Bar name="Market Capacity" dataKey="capacity" fill="#B48DD8" />
-                            <Bar name="Volume Traded (24h)" dataKey="volume" fill="#694BA0" />
-                        </BarChart>
-                    </ResponsiveContainer>
-                </div>
-                <div className="stats-display">
-                    {allCoins}
-                </div>
-            </div>
             )}
         </>
 	)
